18f0ddc83a7c9c5dac373bcfbc4f42a13ae6e6d6
jsistop16 <83804417+jsistop16@users.noreply.github.com>
2021-08-08 22:06:50 +0900

    음악 유튜브 영상 리스트 개인프로젝트

---
+++ MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.singingcoach;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void onButton1Clicked(View v) {
+        Toast.makeText(getApplicationContext(), "좋아요를 눌렀습니다!! 제원이 기분좋앙", Toast.LENGTH_LONG).show();
+    }
+    public void onButton2Clicked(View v) {
+        Toast.makeText(getApplicationContext(), "싫어요를 눌렀습니다!! 힝...", Toast.LENGTH_LONG).show();
+    }
+    public void onButton3Clicked(View v) {
+        Toast.makeText(getApplicationContext(), "좋아요를 눌렀습니다!! 호이짜", Toast.LENGTH_LONG).show();
+    }
+    public void onButton4Clicked(View v) {
+        Toast.makeText(getApplicationContext(), "싫어요를 눌렀습니다!!", Toast.LENGTH_LONG).show();
+    }
+    public void onButton5Clicked(View v) {
+        Toast.makeText(getApplicationContext(), "좋아요를 눌렀습니다!! 뀨우우우", Toast.LENGTH_LONG).show();
+    }
+    public void onButton6Clicked(View v) {
+        Toast.makeText(getApplicationContext(), "싫어요를 눌렀습니다!! 뒤질래 씹창년아", Toast.LENGTH_LONG).show();
+    }
+    public void onButton7Clicked(View v) {
+        Toast.makeText(getApplicationContext(), "좋아요를 눌렀습니다!!", Toast.LENGTH_LONG).show();
+    }
+    public void onButton8Clicked(View v) {
+        Toast.makeText(getApplicationContext(), "싫어요를 눌렀습니다!!", Toast.LENGTH_LONG).show();
+    }
+    public void onButton9Clicked(View v) {
+        Toast.makeText(getApplicationContext(), "좋아요를 눌렀습니다!!", Toast.LENGTH_LONG).show();
+    }
+    public void onButton10Clicked(View v) {
+        Toast.makeText(getApplicationContext(), "싫어요를 눌렀습니다!!", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
